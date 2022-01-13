c558644d23348922ebfe7a55dea4ddca42e9b39b
Michelle Berta <bertafamily@sbcglobal.net>
2022-01-13 17:50:27 -0600

    began working on socring function and simplified html

---
+++ app.js
@@ -0,0 +1,10 @@
+const form = document.getElementsByClassName ('points');
+console.log(form);
+
+
+
+
+const red3 = 100;
+const owned = "";
+const pointsEarned = red3 * owned;
+console.log (pointsEarned)
